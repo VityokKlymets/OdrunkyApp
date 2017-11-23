4ecc17623502d80e7825b4487eebbffddb34cbf7
VityokKlymets <vityokklymets@gmail.com>
2017-11-23 16:14:13 +0200

    some fixes

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -2,59 +2,57 @@ import React from "react";
 import leftImg from "../Content/Images/Icons/ic_chevron_left_48px.svg";
 import rightImg from "../Content/Images/Icons/ic_chevron_right_48px.svg";
 import Preloader from "./Preloader";
-import $ from 'jquery';
+import $ from "jquery";
 class Slider extends React.Component {
   constructor(props) {
     super(props);
     this.Images = props.Images;
     this.currentImage = 0;
     this.state = {
-      imageLoaded: false
-    };
+      imageLoaded : props.Images.map(()=>false)
+    }
   }
   renderImage() {
-    return (
-        this.props.Images.map((elem, idx) => {
-          return (
-            <div
-              key={idx}
-              style={{
-                backgroundImage: `url(${elem})`,
-                left : `${idx*100}%`
-              }}
-
-              className={'slide-image'}
-            >
-            </div>
-          );
-        })
-    );
+    return this.props.Images.map((elem, idx) => {
+      return (
+        <div
+          key={idx}
+          style={{
+            backgroundImage: `url(${elem})`,
+            left: `${idx * 100}%`
+          }}
+          className={"slide-image"}
+        >
+          <img
+            src={this.Images[this.currentImage]}
+            onLoad={() => {
+              let newValue = this.state.imageLoaded.slice();
+              newValue[idx] = true
+              this.setState({imageLoaded : newValue})
+            }}
+            className="hide"
+          />
+          {!this.state.imageLoaded[idx] && this.renderPreloader()}
+        </div>
+      );
+    });
   }
   renderPreloader() {
     return (
-      <div>
-        <Preloader />
-        <img
-          src={this.Images[this.currentImage]}
-          onLoad={() => {
-            this.setState({ imageLoaded: true });
-          }}
-          className="hide"
-        />
-      </div>
+        <Preloader transparent type='small'/>
     );
   }
   renderControls() {
     return (
       <div className="controls-block">
         <div
-          ref='left'
-          className='left disabled'
-          style={{backgroundImage: `url(${leftImg})` }}
+          ref="left"
+          className="left disabled"
+          style={{ backgroundImage: `url(${leftImg})` }}
         />
         <div
-        ref='right'
-          className='right'
+          ref="right"
+          className="right"
           style={{ backgroundImage: `url(${rightImg})` }}
         />
       </div>
@@ -64,9 +62,9 @@ class Slider extends React.Component {
     return (
       <div className="Slider clearfix">
         <div className="slide">
-        {this.renderControls()}
-          <div className="slides" ref='slides'>
-            {this.state.imageLoaded ? this.renderImage() : this.renderPreloader()}
+          {this.renderControls()}
+          <div className="slides" ref="slides">
+              { this.renderImage()}
           </div>
         </div>
         <div className="content">
@@ -81,33 +79,30 @@ class Slider extends React.Component {
       </div>
     );
   }
-  componentDidMount = ()=>{
-    let Slider  = $('.Slider')
-    let slides = Slider.find('.slides'),
-    left = Slider.find('.left'),
-    right = Slider.find('.right');
+  componentDidMount = () => {
+    let Slider = $(".Slider");
+    let slides = Slider.find(".slides"),
+      left = Slider.find(".left"),
+      right = Slider.find(".right");
     const slidesCount = this.props.Images.length;
-    right.bind('click',()=>{
-      if(this.currentImage<slidesCount-1){
+    right.bind("click", () => {
+      if (this.currentImage < slidesCount - 1) {
         this.currentImage++;
-        $(slides).css('left',`-${this.currentImage*100}%`);
-        left.removeClass('disabled')
+        $(slides).css("left", `-${this.currentImage * 100}%`);
+        left.removeClass("disabled");
       }
-    if(this.currentImage===slidesCount-1){
-      right.addClass('disabled');
-    }
-    
-  })
-    left.bind('click',()=>{
-      if(this.currentImage>0){
+      if (this.currentImage === slidesCount - 1) {
+        right.addClass("disabled");
+      }
+    });
+    left.bind("click", () => {
+      if (this.currentImage > 0) {
         this.currentImage--;
-        $(slides).css('left',`-${this.currentImage*100}%`);
-        right.removeClass('disabled');
+        $(slides).css("left", `-${this.currentImage * 100}%`);
+        right.removeClass("disabled");
       }
-      if(this.currentImage===0)
-      left.addClass('disabled')
-      
-    })
-  }
+      if (this.currentImage === 0) left.addClass("disabled");
+    });
+  };
 }
 export default Slider;
